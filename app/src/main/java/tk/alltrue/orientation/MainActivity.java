f9571bb7af70286bebd94519c4afc833a67d1fc0
alx <37true@gmail.com>
2018-11-26 02:19:01 +0700

    get orientation fun

--- app/src/main/java/tk/alltrue/orientation/MainActivity.java
+++ app/src/main/java/tk/alltrue/orientation/MainActivity.java
@@ -1,7 +1,11 @@
 package tk.alltrue.orientation;
 
+import android.content.res.Configuration;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private String getScreenOrientation() {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+            return "Portrait orientation";
+        else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+            return "Landscape orientation";
+        return "";
+    }
+
+    public void onClick(View view) {
+        String orient = getScreenOrientation();
+        TextView or = (TextView) findViewById(R.id.textViewOrient);
+        or.setText(orient);
     }
 }
