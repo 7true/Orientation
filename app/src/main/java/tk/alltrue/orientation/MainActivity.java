a54725c69dd388987468ca64f4200e423b9f0951
alx <37true@gmail.com>
2018-11-26 07:24:30 +0700

    get rotate fun

--- app/src/main/java/tk/alltrue/orientation/MainActivity.java
+++ app/src/main/java/tk/alltrue/orientation/MainActivity.java
@@ -1,18 +1,28 @@
 package tk.alltrue.orientation;
 
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Surface;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    static final String ORIENTATION_PORTRAIT = "portrait";
+    static final String ORIENTATION_LANDSCAPE = "landscape";
+    boolean mState = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button3);
+        mButton.setText(ORIENTATION_LANDSCAPE);
     }
 
     private String getScreenOrientation() {
@@ -51,4 +61,15 @@ public class MainActivity extends AppCompatActivity {
         or.setText(rotation);
     }
 
+    public void onClick3(View view) {
+        if(!mState) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            mButton.setText(ORIENTATION_PORTRAIT);
+        }
+        else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            mButton.setText(ORIENTATION_LANDSCAPE);
+        }
+        mState = !mState;
+    }
 }
